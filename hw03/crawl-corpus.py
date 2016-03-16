523113754d207d604dbcc174a7f2dcb6f9714df5
Dmitry Barashev <dbarashev+committer@ganttproject.biz>
2016-03-16 04:33:43 +0300

    поправлен паук

--- hw03/crawl-corpus.py
+++ hw03/crawl-corpus.py
@@ -1,10 +1,10 @@
+# encoding: utf-8
+import argparse
 import json
 import mwclient
 import mwparserfromhell as mwparser
+import os
 import sys
-sys.path.append("../dfs/")
-
-import client as dfs
 
 def get_authors(page):
 	authors = []
@@ -17,20 +17,34 @@ def get_authors(page):
 		pass
 	return authors
 
-site = mwclient.Site('en.wikipedia.org')
-category = site.Pages['Category:Distributed_file_systems']
-counter = 0
+def crawl(category, dir):
+	site = mwclient.Site('en.wikipedia.org')
+	category = site.Pages[category]
+	counter = 0
 
-encoder = json.JSONEncoder(indent = 2)
-with open("data/shards.txt", "w") as toc:
-	for page in category:
-		print(page.name)
-		page_filename = "data/page%d" % counter
-		with open(page_filename, "w") as f:
-			page_record = {}
-			page_record["Title"] = page.name
-			page_record["Authors"] = get_authors(page)
-			page_record["Body"] = mwparser.parse(page.text()).strip_code().encode('utf-8')
-			f.write(encoder.encode(page_record))
-		toc.write("%s" % page_filename)
-		counter += 1
+	encoder = json.JSONEncoder(indent = 2)
+	with open("%s/shards.txt" % dir, "w") as toc:
+		for page in category:
+			print(page.name)
+			page_filename = "%s/page%d" % (dir, counter)
+			with open(page_filename, "w") as f:
+				page_record = {}
+				page_record["ID"] = counter
+				page_record["Title"] = page.name
+				page_record["Authors"] = get_authors(page)
+				page_record["Body"] = mwparser.parse(page.text()).strip_code().encode('utf-8')
+				f.write(encoder.encode(page_record))
+			toc.write("%s\n" % page_filename)
+			counter += 1
+
+# python crawl-corpus.py --category Category:Distributed_file_systems --dir data
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Стягивает содержимое категории из аглоязычной википедии")
+    parser.add_argument("--category", required = False,  default = 'Category:Distributed_file_systems', help="Название категории")
+    parser.add_argument("--dir", required = False, default = 'data', help="Каталог для записи корпуса")
+    args = parser.parse_args()
+    print("Downloading %s into %s" % (args.category, args.dir))
+    if not os.path.exists(args.dir):
+        os.makedirs(args.dir)
+    crawl(args.category, args.dir)
